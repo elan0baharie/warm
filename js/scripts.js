da0fa1a3a15e4514071a8c8de8b8a6fa39b6fa90
Anatoliy Klots and Elan Baharie <anatoliy@>
2017-03-29 08:37:29 -0700

    make initial setup and first set of branching

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+var leapYear = function(year) {
+  if ((year % 4 === 0) && year % 100 != 0) {
+    return true;
+  } else if () {
+
+  } else {
+    return false;
+  }
+};
+
+
+
+
+//UI
+
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+    $("#result").text(result);
+  });
+});
